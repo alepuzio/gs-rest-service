addfefe08f16f5a3999bf32864dfa5e2f9fbd909
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-02-04 13:04:46 +0100

    added Jackson annotation for the POT and GET

--- complete/src/main/java/com/example/restservice/bean/Greeting.java
+++ complete/src/main/java/com/example/restservice/bean/Greeting.java
@@ -1,19 +1,29 @@
 package com.example.restservice.bean;
 
-public class Greeting {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-	private final long id;
-	private final String content;
+public class Greeting {
+	@JsonProperty("Id")
+	private  long id;
+	@JsonProperty("Content")	
+	private  String content;
 
 	public Greeting(long id, String content) {
 		this.id = id;
 		this.content = content;
 	}
-
+	/*senza costruttore vuoto ho un
+	 * Caused by: com.fasterxml.jackson.databind.exc.InvalidDefinitionException: 
+	 * Cannot construct instance of `com.example.restservice.bean.Greeting` (no Creators, like default constructor, exist): 
+	 * cannot deserialize from Object value (no delegate- or property-based Creator)
+	 * quando chiamo la POST e le GET da client */
+	public Greeting() {
+		super();
+	}
 	public long getId() {
 		return id;
 	}
-
+	
 	public String getContent() {
 		return content;
 	}
