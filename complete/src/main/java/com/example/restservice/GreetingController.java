5f8e1f6536c086261e6875300d3ad8570bd3d55e
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-02-04 11:00:41 +0100

    moved code in other controller

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -20,7 +20,6 @@ public class GreetingController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-
 	@GetMapping("/greetings/{id}")
 	public Greeting singleGreetings(@PathVariable String id) {
 		return new Greeting(counter.incrementAndGet(), String.format("Ciao %s!", id));
@@ -30,28 +29,13 @@ public class GreetingController {
 	public Greeting deleteGreeting(@PathVariable String id) {
 		return new Greeting(new Integer(id), String.format("Eliminato %s!", id));
 	}
-
-	@PutMapping( "/greetings_old/{id}")
-	public Greeting aggiornaGreeting_2(@PathVariable String id, @RequestBody MultiValueMap<String, String> formParams) {
-		return new Greeting(Integer.parseInt(formParams.get("id").get(0)),
-				String.format("Aggiornato contenuto in %s", formParams.get("content")));
-	}
 	
 	@PutMapping("/greetings/{idResource}") 
 	@ResponseBody
 	public ResponseEntity<Greeting> aggiornaGreeting(@PathVariable(value="idResource") String id, @RequestBody Greeting greeting) {
-		//Greeting greeting = new Greeting(-1, "nadadenada");
-		//String id = new Long(greeting.getId()).toString();
 		System.out.println(String.format("aggiornaGreeting(%s,%s)", id, greeting.toString()));
 		return  ResponseEntity.ok(new Greeting(greeting.getId(),
-				String.format("Aggiornato [%s] contenuto in -%s-!", greeting.getId(), greeting.getContent())));
+				String.format("Aggiornato [%s] contenuto in -%s-!", id, greeting.getContent())));
 	}
 
-	/*
-	 * @PostMapping("/greetings") public Greeting newGreeting(@RequestBody
-	 * String body) { final Long number = counter.incrementAndGet(); return new
-	 * Greeting(number.intValue(), String.format("Creato %d!", number)); }
-	 * 
-	 * 
-	 */
 }
