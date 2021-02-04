d68cfca5ef2177ff5d8b867c1b4e299789b5c1e1
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-02-04 17:05:54 +0100

    fixed POST

--- complete/src/test/java/com/example/restservice/GroupGreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GroupGreetingControllerTests.java
@@ -6,11 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,11 +29,14 @@ public class GroupGreetingControllerTests {
 		;
 	}
 
-	//@Test
+	@Test
 	public void postGreeting() throws Exception {
-		this.mockMvc.perform( post("/greetings") )
-		.andDo(print()).andExpect(status().isOk())
-		.andExpect(jsonPath("$.content").value("Creata nuova risorsa!"));
+		MvcResult mvcResult = this.mockMvc.perform(post("/greetings"))
+				.andDo(print()).andExpect(status().isOk())
+					.andReturn();	
+		String result = mvcResult.getResponse().getContentAsString();
+		Assertions.assertEquals("{\"Id\":1,\"Content\":\"Creata nuova risorsa!\"}", result);
+	
 	}
 
 
