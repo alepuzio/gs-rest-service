00b86477c9584d17f27f3c15f9346dc7d35f2e95
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-04-13 16:58:55 +0200

    successfull run

--- complete/src/main/java/net/alepuzio/restservice/server/jwt/JwtUserDetailsService.java
+++ complete/src/main/java/net/alepuzio/restservice/server/jwt/JwtUserDetailsService.java
@@ -1,11 +1,12 @@
 package net.alepuzio.restservice.server.jwt;
-import org.springframework.stereotype.Component;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 /**
  * from https://dzone.com/articles/spring-boot-security-json-web-tokenjwt-hello-world
  * */
 @Component
-public class JwtUserDetailsService {
+public class JwtUserDetailsService implements UserDetailsService {
 	
 	public UserDetails loadUserByUsername(String name){
 		return new User( name);
